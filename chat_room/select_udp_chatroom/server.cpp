11946de17e2e0601d5bc379b8976aaf35d2674d3
cyz6668 <1036605129@qq.com>
2022-05-04 20:36:06 +0800

    Add files via upload

--- chat_room/select_udp_chatroom/server.cpp
+++ chat_room/select_udp_chatroom/server.cpp
@@ -2,19 +2,32 @@
 #include <iostream>
 using namespace std;
 
+/*
+Udp通信：
+双人聊天室
+
+*/
+
 int main()
 {
     int ret;
     char temp;
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     ERROR_CHECK(sockfd, -1, "socket");
+    int on = 1;
+    ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    ERROR_CHECK(ret, -1, "setsockopt");
     struct sockaddr_in sockinfo;
-    socklen_t len = sizeof(sockinfo);
     bzero(&sockinfo, sizeof(sockinfo));
     sockinfo.sin_addr.s_addr = inet_addr("172.21.0.7");
     sockinfo.sin_port = htons(2000);
     sockinfo.sin_family = AF_INET;
-    sendto(sockfd, "1", 1, 0, (sockaddr *)&sockinfo, len);
+    ret = bind(sockfd, (sockaddr *)&sockinfo, sizeof(sockinfo));
+    ERROR_CHECK(ret, -1, "bind");
+    struct sockaddr_in client;
+    socklen_t len = sizeof(client);
+    recvfrom(sockfd, &temp, 1, 0, (sockaddr *)&client, &len);
+    cout << inet_ntoa(client.sin_addr) << ":" << ntohs(client.sin_port) << endl;
 
     fd_set rdsets;
     struct timeval tv;
@@ -33,7 +46,7 @@ int main()
             {
                 char buf[100] = {0};
                 read(0, buf, sizeof(buf));
-                sendto(sockfd, buf, strlen(buf) - 1, 0, (sockaddr *)&sockinfo, len);
+                sendto(sockfd, buf, strlen(buf) - 1, 0, (sockaddr *)&client, len);
             }
             if (FD_ISSET(sockfd, &rdsets))
             {
